11b1fcf74316b62f5e6c775bddca76405bbd3085
Ravinandanhv <112844300+Ravinandanhv@users.noreply.github.com>
2023-04-13 12:57:08 +0530

    Add files via upload

---
+++ test2copy.js
@@ -0,0 +1,106 @@
+var express = require('express');
+const url = require('url');
+var app = express();
+var cors = require('cors')
+app.use(cors())
+// for parsing the body in POST request
+var bodyParser = require('body-parser');
+const ObjectId = require('mongodb').ObjectId;
+const {MongoClient}=require('mongodb');
+const uri = "mongodb+srv://ravinandan:Xx8C3AOm5dRvAtZq@cluster0.klfhs2c.mongodb.net/test";
+const client = new MongoClient(uri);
+const collection = client.db("data-manage").collection("Users");
+  // perform actions on the collection object
+var users =[];
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get('/',(req,res)=>{
+  res.send('port working')
+})
+
+app.get('/api/users', function(req, res){
+    collection.find({}).toArray((error, documents) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.send(documents);
+        }
+      }); 
+});
+
+app.get('/api/users/:id', function(req,res){
+  collection.findOne({_id:ObjectId(req.params.id)}, (error, documents) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send(documents);
+    }
+  }); 
+})
+
+app.post('/api/users', function (req, res) {
+  var user=req.body;
+    collection.insertOne(user, (error, result) => {
+        if (error) {
+          console.log(error);
+        } else {
+          res.send(result)
+          console.log('Document inserted successfully');
+        }
+      });
+});
+
+app.put('/api/users', function(req,res){
+  var user1=req.body;
+  var _ids=req.body._id;
+  delete user1._id;
+  console.log(user1)
+  // collection.updateOne({_id: ObjectId(_id)},{$set:user}, (err, result) => {
+  //   if(result){
+  //     console.log(result)
+  //     res.send(result)
+  //   }
+  // })
+  _ids=_ids.map(ele =>{return ObjectId(ele)});
+  collection.updateMany({_id: {$in :_ids}},{$set:user1},(err,result)=>{
+    if(result){
+      console.log(result.modifiedCount)
+      res.send(result)
+    }else{
+      console.log(err)
+    }
+  })
+})
+
+app.post('/api/users/delete', function(req,res){
+  var ids = req.body;
+  console.log(ids)
+  ids = ids.map(id => {return ObjectId(id)});
+  console.log(ids)
+  collection.deleteMany({_id: { $in: ids }}, (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.send({mess:'Document deleted successfully'})
+      console.log('Document deleted successfully',result);
+    }
+  });
+});
+
+app.post('/login', function(req,res){
+  const cred=client.db("admin").collection("credentials")
+  cred.insertOne(req.body,(err,result)=>{
+    if(err){
+      console.log(err);
+    }else{
+      res.send(result);
+    }
+  })
+})
+
+const port=process.env.PORT || 3000
+app.listen(port, function(){
+    console.log(`Server listening on port ${port}`);
+});
